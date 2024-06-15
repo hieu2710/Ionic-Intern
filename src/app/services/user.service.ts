56e451390d7c1a26962a0df09a4a26a957a50a82
hieu <trunghieu27102002@gmail.com>
2024-06-15 15:53:52 +0700

    fix(NG0200): Comment out the codes related to the two files AuthGuard and Header request

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from '../user';
-import { RelationshopHttpClient } from './header-request.service';
+// import { RelationshopHttpClient } from './header-request.service';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
@@ -28,12 +28,17 @@ export class UserService {
 
   }
 
-  login(username: string): Observable<any> {
-    return this.http.post<any>(`${this.apiUrl}`, { username });
-  }
-  ReadUsers(){
-    this.http.get<any>(`${this.apiUrl}`)
-  }
+  // getUsers(): Observable<User[]> {
+  //   return this.http.get<User[]>(`${this.apiUrl}`);
+
+  // }
+
+  // login(username: string): Observable<any> {
+  //   return this.http.post<any>(`${this.apiUrl}`, { username });
+  // }
+  // ReadUsers(){
+  //   this.http.get<any>(`${this.apiUrl}`)
+  // }
   postUsers(dataSignUp: any): Observable<any> {
     const requestPost = {
       username: dataSignUp.username,
